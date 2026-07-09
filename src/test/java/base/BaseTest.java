09b6eb35fbf934cb89a962882e82e08c6de31fd7
Leri1n <valeriyaer20@gmail.com>
2026-07-09 21:47:56 +0300

    Add automated tests for login, the inventory page, and checkout scenarios.
    Added automated tests for:
    - Login
    - Successful login Invalid username
    - Empty credentials
    - Login without password
    - Inventory
    - Products page title
    - Logout Access restriction after logout  - Cart badge after opening cart
    - Checkout:
    - Successful checkout
    - Continue shopping from cart

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,23 +2,34 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;//Импорт класса ожидания
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import java.time.Duration;//Импорт класса Duration для задания временного ожидания
 
 public class BaseTest {
+    //Переменная браузера. Protected означает, что она доступна во всех классах-наследниках
     protected WebDriver driver;
+    //Позволяет дождаться появления элемента на странице
+    protected WebDriverWait wait;
 
     //Чтобы настроить браузер и открыть его
-    @BeforeMethod
+    @BeforeMethod //Метод выполняется ПЕРЕД каждым тестом
     public void setup(){
+        //Создаём новый экземпляр браузера Chrome
         driver = new ChromeDriver();
+        //Создаем объект явного ожидания. Selenium будет ждать элемент (пока он загрузится) до 10 секунд.
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Открываем тестовый сайт.
         driver.get("https://www.saucedemo.com/");
 
     }
     //Чтобы закрыть браузер
-    @AfterMethod
+    @AfterMethod //Метод выполняется ПОСЛЕ каждого теста
     public void tearDown(){
+        //Проверка, что браузер существует
         if (driver != null){
+            //Полностью закрываем браузер
             driver.quit();
         }
     }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,28 +3,36 @@ package pages;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     private WebDriver driver;
+    private WebDriverWait wait; //Переменная явного ожидания
 
     private By username = By.id("user-name");
     private By password = By.cssSelector("[data-test='password']");
     private By loginButton = By.xpath("//input[@id='login-button']");
     private By errorMessage = By.cssSelector("[data-test='error']");
 
-    public LoginPage(WebDriver driver){
-        this.driver = driver;
+    public LoginPage(WebDriver driver, WebDriverWait wait){
+        this.driver = driver; //Сохраняем браузер
+        this.wait = wait; //Сохраняем ожидание.
     }
+    //Когда создается объект LoginPage, в него передается браузер и ожидание.
+
 
     public void login(String user, String pass){
-        Allure.step("Авторизация пользователя");
-        driver.findElement(username).sendKeys(user);
-        driver.findElement(password). sendKeys(pass);
-        driver.findElement(loginButton).click();
+        Allure.step("Авторизация пользователя"); //Добавляем шаг в отчет Allure
+        wait.until(ExpectedConditions.visibilityOfElementLocated(username)); //Ждем пока поле Username загрузится
+        driver.findElement(username).sendKeys(user); //Вводим логин.
+        driver.findElement(password). sendKeys(pass); //Вводим пароль.
+        driver.findElement(loginButton).click(); //Нажимаем Login.
     }
 
     public String getErrorText(){
-        Allure.step("Получение текста ошибки");
+        Allure.step("Получение текста ошибки"); //Добавляем шаг в отчет Allure.
+        wait.until(ExpectedConditions.visibilityOfElementLocated(errorMessage)); // Ждем появления сообщения об ошибке.
         return driver.findElement(errorMessage).getText();
     }
 }
